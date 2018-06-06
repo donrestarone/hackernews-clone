58a54e5a567af83906cd6d28ccc32df94c996bb8
donrestarone <shashikejayatunge@gmail.com>
2018-06-06 17:12:25 -0400

    all information shows up

--- hackernewsClone/app/assets/javascripts/index.js
+++ hackernewsClone/app/assets/javascripts/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 	// make request to internal api
 	var request = new XMLHttpRequest();
 
-	request.open('GET', url, true);
+	request.open('GET', localHost, true);
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			console.log('success');
@@ -34,11 +34,18 @@ document.addEventListener("DOMContentLoaded", function(event){
 				var articleUl = document.createElement('ul');
 				var articleLi = document.createElement('li');
 				var articleLinkliWithTitle = document.createElement('a');
+				var authorPTag = document.createElement('p');
+				if ("kids" in article) {
+					authorPTag.innerText = article.score + ' points|' + ' by ' + article.by + ' ' + article.kids.length + ' comments'
+				} else {
+					authorPTag.innerText = article.score + ' points|' + ' by ' + article.by + ' no comments'
+				}
 
-				articleLinkliWithTitle.src = article.url;
+				articleLinkliWithTitle.setAttribute('href', article.url);
 				articleLinkliWithTitle.innerText = article.title;
 
 				articleLi.appendChild(articleLinkliWithTitle);
+				articleLi.appendChild(authorPTag);
 				articleList.append(articleLi);
 				safe = true
 				
@@ -49,43 +56,67 @@ document.addEventListener("DOMContentLoaded", function(event){
 	}	
 
 
-			// testing scroll bottom
-			document.addEventListener('scroll', function (event) {
-			    if (document.body.scrollHeight == 
-			        document.body.scrollTop +        
-			        window.innerHeight && safe === true) {
-			    	count += 30
-			    	alert('bottom');
-					
-					var request = new XMLHttpRequest();
-					var infiniteUrlLocal = 'http://localhost:3000/articles/api?count=' + count + '&type=topstories';
-					var infiniteUrl = 'https://clone-hackernews.herokuapp.com/articles/api?count=' + count + '&type=topstories';
-					request.open('GET', infiniteUrl, true);
-					request.onload = function() {
-						if (request.status >= 200 && request.status < 400) {
-							console.log('success');
-							var data = request.responseText
-							var dataObj = JSON.parse(data);
-							dataObj.forEach(function(article){
-								var articleUl = document.createElement('ul');
-								var articleLi = document.createElement('li');
-								var articleLinkliWithTitle = document.createElement('a');
-
-								articleLinkliWithTitle.src = article.url;
-
-								articleLinkliWithTitle.innerText = article.title;
-
-								articleLi.appendChild(articleLinkliWithTitle);
-								articleList.append(articleLi);
-								console.log('article count is' + count)
-							})
+	document.addEventListener=document.addEventListener || document.attachEvent;
+	document.addEventListener('scroll',function(ev){
+	    var st = getScrollTop();
+	    if(!st){
+	            console.log('top');
+	    }
+	    if((st+document.documentElement.clientHeight)>=document.documentElement.scrollHeight ){
+	            console.log('bottom');
+			count += 30
+	    	alert('bottom');
+			
+			var request = new XMLHttpRequest();
+			var infiniteUrlLocal = 'http://localhost:3000/articles/api?count=' + count + '&type=topstories';
+			var infiniteUrl = 'https://clone-hackernews.herokuapp.com/articles/api?count=' + count + '&type=topstories';
+			request.open('GET', infiniteUrlLocal, true);
+			request.onload = function() {
+				if (request.status >= 200 && request.status < 400) {
+					console.log('success');
+					var data = request.responseText
+					var dataObj = JSON.parse(data);
+					dataObj.forEach(function(article){
+						var articleUl = document.createElement('ul');
+						var articleLi = document.createElement('li');
+						var articleLinkliWithTitle = document.createElement('a');
+						var authorPTag = document.createElement('p');
+						if ("kids" in article) {
+							authorPTag.innerText = article.score + ' points|' + ' by ' + article.by + ' ' + article.kids.length + ' comments'
 						} else {
-							console.log('fail');
+							authorPTag.innerText = article.score + ' points|' + ' by ' + article.by + ' no comments'
 						}
-					}
-					request.send()
-			    }
-			});
+
+						articleLinkliWithTitle.setAttribute('href', article.url);
+						articleLinkliWithTitle.innerText = article.title;
+
+						articleLi.appendChild(articleLinkliWithTitle);
+						articleLi.appendChild(authorPTag);
+						articleList.append(articleLi);
+						console.log('article count is' + count)
+					})
+				} else {
+					console.log('fail');
+				}
+			}
+			request.send()
+	    }                     
+	});
+
+
+	function getScrollTop(){
+	    return Math.max(document.documentElement.scrollTop,document.body.scrollTop);
+	}
+
+
+	// testing scroll bottom
+	document.addEventListener('scroll', function (event) {
+	    if (document.body.scrollHeight == 
+	        document.body.scrollTop +        
+	        window.innerHeight && safe === true) {
+	    	
+	    }
+	});
 
 	//listener for the bottom of the page
 
