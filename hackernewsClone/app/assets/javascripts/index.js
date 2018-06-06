c3aebc39b20e4ef4728e382267074ce772f456c4
donrestarone <shashikejayatunge@gmail.com>
2018-06-06 16:05:08 -0400

    reconcile local/web urls

--- hackernewsClone/app/assets/javascripts/index.js
+++ hackernewsClone/app/assets/javascripts/index.js
@@ -7,7 +7,7 @@ document.addEventListener("DOMContentLoaded", function(event){
 	// make request to internal api
 	var request = new XMLHttpRequest();
 
-	request.open('GET', localHost, true);
+	request.open('GET', url, true);
 	request.onload = function() {
 		if (request.status >= 200 && request.status < 400) {
 			console.log('success');
@@ -59,8 +59,8 @@ document.addEventListener("DOMContentLoaded", function(event){
 					
 					var request = new XMLHttpRequest();
 					var infiniteUrlLocal = 'http://localhost:3000/articles/api?count=' + count + '&type=topstories';
-					
-					request.open('GET', infiniteUrlLocal, true);
+					var infiniteUrl = 'https://clone-hackernews.herokuapp.com/articles/api?count=' + count + '&type=topstories';
+					request.open('GET', infiniteUrl, true);
 					request.onload = function() {
 						if (request.status >= 200 && request.status < 400) {
 							console.log('success');
