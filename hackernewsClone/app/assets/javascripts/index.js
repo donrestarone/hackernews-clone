0a51e420e2f19e0973b64befe0350388d8d9c793
donrestarone <shashikejayatunge@gmail.com>
2018-06-06 15:54:48 -0400

    need to fix scrolling for chrome/firefox

--- hackernewsClone/app/assets/javascripts/index.js
+++ hackernewsClone/app/assets/javascripts/index.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event){
-	var url = 'https://clone-hackernews.herokuapp.com/articles/api?count=90&type=topstories';
-	var localHost = 'http://localhost:3000/articles/api?count=90&type=topstories';
+	var count = 30
+	var url = 'https://clone-hackernews.herokuapp.com/articles/api?count=' + count + '&type=topstories';
+	var localHost = 'http://localhost:3000/articles/api?count=' + count + '&type=topstories';
 	var safe = false
 	
 	// make request to internal api
@@ -19,11 +20,11 @@ document.addEventListener("DOMContentLoaded", function(event){
 	// add listener for the ready state change
 	request.addEventListener("readystatechange", processRequest, false);
 	// request.onreadystatechange = processRequest;
-
+	var articleList = document.createElement('ul');
 	// process the response
 	function processRequest(e) {
 		if (request.readyState === 4 && request.status === 200) {
-			var articleList = document.createElement('ul');
+			
 			var body = document.querySelector('.container')
 			body.append(articleList);
 
@@ -35,7 +36,6 @@ document.addEventListener("DOMContentLoaded", function(event){
 				var articleLinkliWithTitle = document.createElement('a');
 
 				articleLinkliWithTitle.src = article.url;
-				console.log(articleLinkliWithTitle.src)
 				articleLinkliWithTitle.innerText = article.title;
 
 				articleLi.appendChild(articleLinkliWithTitle);
@@ -44,32 +44,46 @@ document.addEventListener("DOMContentLoaded", function(event){
 				
 			});
 			
-
+			
 		}
 	}	
 
-	// var loadMore = function(){
-	// 	for (var i = 0; i < 30; i++) {
 
-	// 	}
-	// }
 			// testing scroll bottom
 			document.addEventListener('scroll', function (event) {
 			    if (document.body.scrollHeight == 
 			        document.body.scrollTop +        
 			        window.innerHeight && safe === true) {
+			    	count += 30
 			    	alert('bottom');
+					
 					var request = new XMLHttpRequest();
-
-					request.open('GET', localHost, true);
+					var infiniteUrlLocal = 'http://localhost:3000/articles/api?count=' + count + '&type=topstories';
+					
+					request.open('GET', infiniteUrlLocal, true);
 					request.onload = function() {
 						if (request.status >= 200 && request.status < 400) {
 							console.log('success');
+							var data = request.responseText
+							var dataObj = JSON.parse(data);
+							dataObj.forEach(function(article){
+								var articleUl = document.createElement('ul');
+								var articleLi = document.createElement('li');
+								var articleLinkliWithTitle = document.createElement('a');
+
+								articleLinkliWithTitle.src = article.url;
+
+								articleLinkliWithTitle.innerText = article.title;
+
+								articleLi.appendChild(articleLinkliWithTitle);
+								articleList.append(articleLi);
+								console.log('article count is' + count)
+							})
 						} else {
 							console.log('fail');
 						}
 					}
-					processRequest()
+					request.send()
 			    }
 			});
 
